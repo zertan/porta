009e9ad3053bafb6c68a490d4c959b98633b1b48
Guilherme Cassolato <guicassolato@gmail.com>
2019-12-18 11:53:28 +0100

    rbtrace

--- config/initializers/gc.rb
+++ config/initializers/gc.rb
@@ -1,3 +1,5 @@
+require 'rbtrace' if ActiveRecord::Type::Boolean.new.type_cast_from_user(ENV.fetch('RBTRACE_ENABLED', false))
+
 if defined?(GC)
   GC.enable_stats if GC.respond_to?(:enable_stats)
 
