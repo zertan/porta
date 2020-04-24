a24761884a47452439ff1c5708bd40cbaa1f26c7
Jose Miguel Gallas <josemigallas@gmail.com>
2020-04-24 11:35:21 +0200

    adds filter chips

--- portafly/src/components/developer-accounts/SearchWidget.tsx
+++ portafly/src/components/developer-accounts/SearchWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useMemo } from 'react'
 import {
   SelectOption,
   Select,
@@ -19,44 +19,34 @@ interface ISearch {
 
 const SearchWidget: React.FunctionComponent<ISearch> = ({ onFilter }) => {
   const { t } = useTranslation('accounts')
+  console.log(onFilter)
 
   const group = t('accounts_table.col_group')
   const admin = t('accounts_table.col_admin')
   const state = t('accounts_table.col_state')
-  const options = [group, admin, state]
+  const categories = [group, admin, state]
 
-  const [isExpanded, setIsExpanded] = useState(false)
-  const [filterBy, setFilterBy] = useState(options[0])
-  const [term, setTerm] = useState('')
-  const textInputRef = useRef<HTMLInputElement>(null)
-
-  const onSelectCategory = (_: any, value: string | SelectOptionObject) => {
-    setFilterBy(value as string)
-    setIsExpanded(!isExpanded)
-
-    textInputRef.current?.focus()
-  }
-
-  const onKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      onFilter(term, filterBy)
-    }
-  }
-
-  const [isStateSelectOpen, setIsStateSelectOpen] = useState(false)
+  const [isCategorySelectExpanded, setIsCategorySelectExpanded] = useState(false)
+  const [isStateSelectExpanded, setIsStateSelectExpanded] = useState(false)
   const [chips, setChips] = useState<Record<string, string[]>>({
     [group]: [],
     [admin]: [],
     [state]: []
   })
+  const [filterBy, setFilterBy] = useState(categories[0])
+  const textInputRef = useRef<HTMLInputElement>(null)
 
-  const onSelectState = (ev: any, selection: string | SelectOptionObject) => {
-    const { checked } = ev.currentTarget as HTMLInputElement
-    const newStateChips = checked
-      ? [...chips[state], selection as string]
-      : chips[state].filter((s) => s !== selection)
+  const onSelectCategory = (_: any, value: string | SelectOptionObject) => {
+    setFilterBy(value as string)
+    setIsCategorySelectExpanded(false)
 
-    setChips({ ...chips, [state]: newStateChips })
+    textInputRef.current?.focus()
+  }
+
+  // TODO: prevent adding the same term twice for categories: group and admin
+  const addChip = (category: string, term: string) => {
+    const newCategoryChips = [...chips[category], term]
+    setChips({ ...chips, [category]: newCategoryChips })
   }
 
   const removeSelection = (category: string, chip: string | DataToolbarChip) => {
@@ -64,83 +54,88 @@ const SearchWidget: React.FunctionComponent<ISearch> = ({ onFilter }) => {
     setChips({ ...chips, [category]: newCategoryChips })
   }
 
+  const onSelectState = (ev: any, selection: string | SelectOptionObject) => {
+    const { checked } = ev.currentTarget as HTMLInputElement
+
+    if (checked) {
+      addChip(state, selection as string)
+    } else {
+      removeSelection(state, selection as string)
+    }
+  }
+
+  const onClick = () => {
+    if (textInputRef.current?.value) {
+      addChip(filterBy, textInputRef.current.value)
+      textInputRef.current.value = ''
+    }
+  }
+
+  const dataToolbarFilters = useMemo(() => categories.map((o) => (
+    <DataToolbarFilter
+      key={o}
+      chips={chips[o]}
+      deleteChip={removeSelection}
+      categoryName={o}
+    >
+      <span />
+    </DataToolbarFilter>
+  )), [chips])
+
+
+  const searchBar = (
+    <InputGroup>
+      <TextInput
+        ref={textInputRef}
+        type="search"
+        aria-label={t('accounts_table.data_toolbar.search_widget.text_input_aria_label')}
+        placeholder={t('accounts_table.data_toolbar.search_widget.placeholder', { option: filterBy.toLowerCase() })}
+        onKeyUp={(ev) => ev.key === 'Enter' && onClick()}
+      />
+      <Button
+        variant={ButtonVariant.control}
+        aria-label={t('accounts_table.data_toolbar.search_widget.button_aria_label')}
+        onClick={onClick}
+      >
+        <SearchIcon />
+      </Button>
+    </InputGroup>
+  )
+
+  const stateSelect = (
+    <Select
+      variant={SelectVariant.checkbox}
+      aria-label="Status"
+      onSelect={onSelectState}
+      selections={chips[state]}
+      isExpanded={isStateSelectExpanded}
+      onToggle={setIsStateSelectExpanded}
+      placeholderText="Status"
+    >
+      <SelectOption key="approved" value="Approved" />
+      <SelectOption key="pending" value="Pending" />
+      <SelectOption key="rejected" value="Rejected" />
+      <SelectOption key="suspended" value="Suspended" />
+    </Select>
+  )
+
   return (
     <DataToolbarGroup variant="filter-group">
       <Select
         toggleIcon={<FilterIcon />}
         variant={SelectVariant.single}
         aria-label={t('accounts_table.data_toolbar.search_widget.select_aria_label')}
-        onToggle={setIsExpanded}
         onSelect={onSelectCategory}
         selections={filterBy}
-        isExpanded={isExpanded}
+        isExpanded={isCategorySelectExpanded}
+        onToggle={setIsCategorySelectExpanded}
         ariaLabelledBy="title-id"
         isDisabled={false}
       >
-        {options.map((o) => <SelectOption key={o} value={o} />)}
+        {categories.map((o) => <SelectOption key={o} value={o} />)}
       </Select>
-      <DataToolbarFilter chips={chips[group]} deleteChip={removeSelection} categoryName={group}>
-        {filterBy === group && (
-          <InputGroup>
-            <TextInput
-              ref={textInputRef}
-              type="search"
-              aria-label={t('accounts_table.data_toolbar.search_widget.text_input_aria_label')}
-              placeholder={t('accounts_table.data_toolbar.search_widget.placeholder', { option: filterBy.toLowerCase() })}
-              value={term}
-              onChange={setTerm}
-              onKeyUp={onKeyUp}
-            />
-            <Button
-              variant={ButtonVariant.control}
-              aria-label={t('accounts_table.data_toolbar.search_widget.button_aria_label')}
-              onClick={() => onFilter(term, filterBy)}
-            >
-              <SearchIcon />
-            </Button>
-          </InputGroup>
-        )}
-      </DataToolbarFilter>
-      <DataToolbarFilter chips={chips[admin]} deleteChip={removeSelection} categoryName={admin}>
-        {filterBy === admin && (
-          <InputGroup>
-            <TextInput
-              ref={textInputRef}
-              type="search"
-              aria-label={t('accounts_table.data_toolbar.search_widget.text_input_aria_label')}
-              placeholder={t('accounts_table.data_toolbar.search_widget.placeholder', { option: filterBy.toLowerCase() })}
-              value={term}
-              onChange={setTerm}
-              onKeyUp={onKeyUp}
-            />
-            <Button
-              variant={ButtonVariant.control}
-              aria-label={t('accounts_table.data_toolbar.search_widget.button_aria_label')}
-              onClick={() => onFilter(term, filterBy)}
-            >
-              <SearchIcon />
-            </Button>
-          </InputGroup>
-        )}
-      </DataToolbarFilter>
-      <DataToolbarFilter chips={chips[state]} deleteChip={removeSelection} categoryName={state}>
-        {filterBy === state && (
-          <Select
-            variant={SelectVariant.checkbox}
-            aria-label="Status"
-            onToggle={() => setIsStateSelectOpen(!isStateSelectOpen)}
-            onSelect={onSelectState}
-            selections={chips[state]}
-            isExpanded={isStateSelectOpen}
-            placeholderText="Status"
-          >
-            <SelectOption key="approved" value="Approved" />
-            <SelectOption key="pending" value="Pending" />
-            <SelectOption key="rejected" value="Rejected" />
-            <SelectOption key="suspended" value="Suspended" />
-          </Select>
-        )}
-      </DataToolbarFilter>
+      {dataToolbarFilters}
+      {filterBy === state ? stateSelect : searchBar}
     </DataToolbarGroup>
   )
 }
